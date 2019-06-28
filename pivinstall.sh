3b559eb882d311a0936a25db6a68c310393e4e27
everythingeverywhere <ivan0tarin@gmail.com>
2019-06-28 09:27:57 -0600

    kubectl and fly install now working

--- pivinstall.sh
+++ pivinstall.sh
@@ -2,6 +2,11 @@
 #Homebrew install
 printf -- 'Installing Hombebrew... \n ' && \
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && \
+#CF CLI Install
+printf -- 'Installing CF CLI... \n' && \
+brew tap cloudfoundry/tap  && \
+brew install cf-cli  && \
+cf --help && \
 #openJDK8 Install
 printf -- 'Intalling OpenJDK v8 ... \n'
 brew cask install adoptopenjdk && \
@@ -18,22 +23,18 @@ printf -- 'Installing terraform... \n ' && \
 brew install terraform
 #Fly CLI v5.3.0
 printf -- 'Downloading Fly CLI .tar file... \n' && \
-curl  -O https://github.com/concourse/concourse/releases/download/v5.3.0/fly-5.3.0-darwin-amd64.tgz && \
-printf -- 'Installing Fly CLI... \n ' && \
+curl  -OL https://github.com/concourse/concourse/releases/download/v5.3.0/fly-5.3.0-darwin-amd64.tgz && \
+printf -- 'Unzipping Fly CLI... \n ' && \
 tar -zxvf fly-5.3.0-darwin-amd64.tgz && \
-printf -- 'Moving Fly CLI to /usr/local/bin/fly \n ...' && \
-sudo mv fly-5.3.0-darwin-amd64.tgz /usr/local/bin/fly && \
+printf -- 'Moving fly directory to /usr/local/bin/fly \n ...' && \
+sudo mv fly /usr/local/bin/ && \
+printf -- 'What's the fly version? && \
 fly --version && \
-#CF CLI Install
-printf -- 'Installing CF CLI... \n' && \
-brew tap cloudfoundry/tap  && \
-brew install cf-cli  && \
-cf --help && \
 #Kubectl Install
-printf -- 'Installing kubeCUTL or kube C. T. L. ... \n' && \
-curl -O https://storage.googleapis.com/kubernetes-release/release/v1.4.1/bin/darwin/amd64/kubectl
+printf -- 'Downloading kubeCUTL or kube C. T. L. ... \n' && \
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.5/bin/darwin/amd64/kubectl
 chmod +x ./kubectl && \
-sudo mv ./kubectl /usr/local/bin/kubectl && \
+sudo mv ./kubectl /usr/local/bin/ && \
 kubectl version && \
 brew cleanup && \
-printf -- '\n';
+printf -- 'end of pivinstall.sh script \n'
